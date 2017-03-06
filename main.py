a7246cd5386fd8f4d8e9bffd8aad52bac9668345
Lucian Andercou <lucian.andercou@gmail.com>
2017-03-06 22:26:41 +0200

    Site updated with BootStrap template

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/butwaittheresmore')
-def more():
-    return render_template('more.html')
-
+    return render_template('samplestrap.html')
+@app.route('/game')
+def game():
+    return render_template('game.html')
 if __name__ == '__main__':
     app.run(debug=True)
