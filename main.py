38c34cdb4728d294de037c16a9c46905b615b8ef
Lucian Andercou <lucian.andercou@gmail.com>
2017-03-10 00:03:04 +0200

    Uninstalled flask-triangle

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-from flask_triangle import Triangle
 from flask_jsglue import JSGlue
 import facebook_scrape_events as f
 
 app = Flask(__name__)
 Bootstrap(app)
-Triangle(app)
 jsglue = JSGlue(app)
 @app.route('/')
 def home():
@@ -16,13 +14,13 @@ def home():
 def game():
     return render_template('game.html')
 
-# @app.route('/gigs')
-# def scrape():
-#     return f.scrapeFacebookEvents(request.query_string.decode("utf-8"))
-#
-# @app.route('/gigsearch')
-# def gigsearch():
-#     return render_template('index1.html')
+@app.route('/gigs')
+def scrape():
+    return f.scrapeFacebookEvents(request.query_string.decode("utf-8"))
+
+@app.route('/gigsearch')
+def gigsearch():
+    return render_template('index1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
